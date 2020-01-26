bf6d4ab33f9a13e828b3fc1bf006511350d6f4f6
iphayao <phayaob@gmail.com>
2020-01-26 21:48:12 +0700

    Add Demo for Actuator Service

---
+++ demo-actuator-service/src/main/java/com/iphayao/demo/DemoActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iphayao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoActuatorServiceApplication.class, args);
+	}
+
+}
---
+++ demo-actuator-service/src/test/java/com/iphayao/demo/DemoActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iphayao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
