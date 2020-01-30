88d6034246013a5e2191b61ccea4867f041c6964
iphayao <phayaob@gmail.com>
2020-01-30 21:43:06 +0700

    Refactor rest example to greeting example

--- demo-greeting-service/src/main/java/com/iphayao/demo/DemoGreetingServiceApplication.java
+++ demo-greeting-service/src/main/java/com/iphayao/demo/DemoGreetingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoRestServiceApplication {
+public class DemoGreetingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoRestServiceApplication.class, args);
+		SpringApplication.run(DemoGreetingServiceApplication.class, args);
 	}
 
 }
